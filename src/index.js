bcd821560dab0029a87f934f976ecf92d5e7830e
sardorbek_saydakbarov <saydakbarovsardor99@gmail.com>
2025-11-27 08:37:38 +0300

    integration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // ⚠️ diqqat
 import App from "./App";
-import '../index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
